716684f6a239eba6ae5f02a62694749a6fd92750
turulomio <turulomio@60adb29d-d142-4147-80d8-a493bdb2a643>
2013-02-08 07:49:00 +0000

    Versión 20130208

--- libdidyoureadme.py
+++ libdidyoureadme.py
@@ -2,7 +2,7 @@ import os,  datetime,  configparser,  hashlib,   psycopg2,  psycopg2.extras,  py
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
-version="20130116"
+version="20130208"
 
 dirTmp=os.path.expanduser("/tmp/didyoureadme/")
 dirDocs=os.path.expanduser("~/.didyoureadme/docs/")
